9b7234c8d788fa9053d1614f6680c2674b5745bf
KrishnaprabhaCM <102539266+KrishnaprabhaCM@users.noreply.github.com>
2022-07-24 19:58:47 +0530

    footer
    created footer component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
     LoginComponent,
     SignUpComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
