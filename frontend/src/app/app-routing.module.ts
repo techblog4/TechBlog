3e9061b7877246b3301d32c81563744000358055
KrishnaprabhaCM <102539266+KrishnaprabhaCM@users.noreply.github.com>
2022-07-24 18:56:23 +0530

    components commit
    created login, sign up and home components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'sign-up',component:SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
