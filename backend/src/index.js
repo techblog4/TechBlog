a4150f88592a415d41a804b116535206550d65dd
Thejus Rajendran <102720972+surya-gayathri@users.noreply.github.com>
2022-07-29 15:05:26 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
