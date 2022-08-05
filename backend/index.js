9eb6f41baf5d5f7988a2ccf8cc884fd8fd5897a5
Arya Krishna K <aryakrishna9946@gmail.com>
2022-08-05 06:17:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express=require("express");
+const express = require("express");
 const cors = require("cors");
 const jwt =require("jsonwebtoken");
 const signupmongo=require("./src/model/signup");
