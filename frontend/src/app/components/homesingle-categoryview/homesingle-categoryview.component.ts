cfa3b4fc8cf00761ef56c85ac997364f22952361
Jisnaaj <102594767+Jisnaaj@users.noreply.github.com>
2022-08-17 23:00:51 +0530

    css

---
+++ frontend/src/app/components/homesingle-categoryview/homesingle-categoryview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homesingle-categoryview',
+  templateUrl: './homesingle-categoryview.component.html',
+  styleUrls: ['./homesingle-categoryview.component.css']
+})
+export class HomesingleCategoryviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
